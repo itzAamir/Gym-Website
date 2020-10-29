7a06cb428d62a5f072caf617d15d7651c93c24d2
itzAamir <amirpc190320@gmail.com>
2020-10-29 18:28:01 +0530

    Added Contact Form

--- js/main.js
+++ js/main.js
@@ -17,3 +17,24 @@ document.querySelector(".btnR").addEventListener("mouseover", ()=>{
 document.querySelector(".btnR").addEventListener("mouseout", ()=>{
     document.getElementById("exploreBtn").style.color = "white";
 })
+
+let links = document.querySelectorAll("li");
+Array.from(links).forEach(elements => {
+    elements.addEventListener("mouseover", changeActive); //changeActive is changing state of "Home" link to inactive
+    elements.addEventListener("mouseout", revertActive);  //revertActive is changing state of "Home" link to active
+});
+
+
+function changeActive(e){
+    elem = e.target.innerText;
+    if (elem != "Home"){
+        document.querySelector(".active").classList.remove("active");
+    }
+}
+
+function revertActive(e){
+    elem = e.target.innerText;
+    if (elem != "Home"){
+        document.querySelector("#active").classList.add("active");
+    }
+}
